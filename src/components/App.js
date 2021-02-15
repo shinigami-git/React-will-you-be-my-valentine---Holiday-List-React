5e4b81cd1367962d5b0c7d270fc7640435855eaf
shinigami-git <75805201+shinigami-git@users.noreply.github.com>
2021-02-16 00:36:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,42 +11,42 @@ class App extends Component {
     { name: 'New York', country: 'USA' },
     { name: 'Darjeeling', country: 'India' },
     { name: 'Tokyo', country: 'Japan' },
-    { name: 'Lonavala', country: 'India' },
-    { name: 'Brandenburg Gate', country: 'Germany' },
-    { name: 'Reichstag Building', country: 'Germany' },
-    { name: 'Museum Island', country: 'Germany' },
-    { name: 'Munnar', country: 'India' },
-    { name: 'Leh Ladakh', country: 'India' },
-    { name: 'Goa', country: 'India' },
-    { name: 'Agra', country: 'India' },
-    { name: 'Dalhousie', country: 'India' },
-    { name: 'Coorg', country: 'India' },
-    { name: 'Rome', country: 'Italy' },
-    { name: 'Milan', country: 'Italy' },
-    { name: 'Venice', country: 'Italy' },
-    { name: 'Varanasai', country: 'India' },
-    { name: 'Jaipur', country: 'India' },
-    { name: 'The Hofburg', country: 'Austria' },
-    { name: 'Belvedere Palace', country: 'Austria' },
-    { name: 'St. Stephen Cathedral', country: 'Austria' },
-    { name: 'Kahna National Park', country: 'India' },
-    { name: 'Amritsar', country: 'India' },
-    { name: 'Mussoorie', country: 'India' },
-    { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
-    ]
+    { name: 'Lonavala', country: 'India' },]
+//     { name: 'Brandenburg Gate', country: 'Germany' },
+//     { name: 'Reichstag Building', country: 'Germany' },
+//     { name: 'Museum Island', country: 'Germany' },
+//     { name: 'Munnar', country: 'India' },
+//     { name: 'Leh Ladakh', country: 'India' },
+//     { name: 'Goa', country: 'India' },
+//     { name: 'Agra', country: 'India' },
+//     { name: 'Dalhousie', country: 'India' },
+//     { name: 'Coorg', country: 'India' },
+//     { name: 'Rome', country: 'Italy' },
+//     { name: 'Milan', country: 'Italy' },
+//     { name: 'Venice', country: 'Italy' },
+//     { name: 'Varanasai', country: 'India' },
+//     { name: 'Jaipur', country: 'India' },
+//     { name: 'The Hofburg', country: 'Austria' },
+//     { name: 'Belvedere Palace', country: 'Austria' },
+//     { name: 'St. Stephen Cathedral', country: 'Austria' },
+//     { name: 'Kahna National Park', country: 'India' },
+//     { name: 'Amritsar', country: 'India' },
+//     { name: 'Mussoorie', country: 'India' },
+//     { name: 'Mount Abu', country: 'India' },
+//     { name: 'Tirupati', country: 'India' },
+//     ]
   }
 
   render() {
-    let arr=[];
-    this.cityList.map((city)=>arr.push(city.name));
-    let filteredIndianCity=this.cityList.filter((city,index)=>(city.country=='India' && arr.indexOf(city.name)==index));
-    let filteredNonIndianCity=this.cityList.filter((city)=>(city.country !=='India' ));
-    let updatedList = [...filteredIndianCity, ...filteredNonIndianCity];
+    //let arr=[];
+    //this.cityList.map((city)=>arr.push(city.name));
+    let filteredIndianCity=this.cityList.filter((city,index)=>(city.country=='India' ));
+    //let filteredNonIndianCity=this.cityList.filter((city)=>(city.country !=='India' ));
+    let updatedList = [...filteredIndianCity];
     return (
       <div id="main">
         <ol>
-            {updatedList.map((city,index)=> <li key={`location${index+1}`}>{city.name}</li>)}
+            {updatedList.map((city,index)=> <li key={`location${index+1}`}>{city.name}(India)</li>)}
         </ol>
         {/* Do not remove the main div */}
       </div>
